3d1ca8794f5776d6c9362c60d3cbcba33a3f1cb7
John Galambos <johnny.galambos@gmail.com>
2018-12-04 16:58:30 -0800

    Parsing claims working

--- Day3/Day3.fsx
+++ Day3/Day3.fsx
@@ -1,3 +1,6 @@
+open System.Text.RegularExpressions
+
+
 let input1 = "#123 @ 3,2: 5x4"
 
 type Claim  =
@@ -8,6 +11,14 @@ type Claim  =
       height: int }
 
 // parseInput string -> claim
+let parseInput (input: string) =
+    let result = Regex.Match(input, "^\#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$")
+    { Id = result.Groups.[1].Value |> int
+      offsetX = result.Groups.[2].Value |> int
+      offsetY = result.Groups.[3].Value |> int
+      width = result.Groups.[4].Value |> int
+      height = result.Groups.[5].Value |> int }
 
 
+parseInput input1
 
