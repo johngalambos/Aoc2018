0f83c1284ddc03d65f33ec827d85aed28c4c91e6
John Galambos <johnny.galambos@gmail.com>
2018-12-06 16:48:08 -0800

    Day 6 wip

--- Day6/day6.fsx
+++ Day6/day6.fsx
@@ -1,18 +1,23 @@
+open System
+
 type Extents =
   { xmin: int
     xmax: int
     ymin: int
     ymax: int }
 
-type TileValue = Tie of int | Closest of int*int
+type ClosestCandidate =
+  { Id: int
+    Dist: int }
+
+
+type TileValue = Tie of int | Closest of ClosestCandidate
 
 type Coord = int*int
 
 type CoordDist =
   { coord: Coord
     dist: int }
-
-let testCoords = [ (1, 1); (1, 6); (8, 3); (3, 4); (5, 5); (8, 9) ]
 
 let findExtents coords =
   let xmin = fst ((List.minBy fst) coords)
@@ -34,70 +39,97 @@ let createCoordGrid extents (x, y) =
           { coord =  (i, j)
             dist = abs (x - i) + abs (j - y) } }
 
-let extents = findExtents testCoords
 
 // overlayCoordGrid :: Map<Coord, TileValue> -> (CoordDist) -> Map<Coord, TileValue>
 let overlayCoordGrid map coordGrid coordId =
   //fold over coordgrid popluting the map
   Seq.fold
-    (fun (grid:Map<Coord, TileValue>) c ->
-      match Map.containsKey c.coord grid with
+    (fun (grid:Map<Coord, TileValue>) cand ->
+      match Map.containsKey cand.coord grid with
       | true ->
-        match Map.find c.coord grid with
-        | Tie d when d > c.dist -> Map.add c.coord (Closest (c.dist, coordId)) grid
-        | Closest (d, _) when d > c.dist -> Map.add c.coord (Closest (c.dist, coordId)) grid
-        | Closest (d, _) when d = c.dist -> Map.add c.coord (Tie c.dist) grid
+        match Map.find cand.coord grid with
+        | Tie d when d > cand.dist ->
+            Map.add cand.coord (Closest { Dist = cand.dist; Id = coordId }) grid
+        | Closest current  when current.Dist > cand.dist ->
+            Map.add cand.coord (Closest { Dist = cand.dist; Id = coordId }) grid
+        | Closest current  when current.Dist = cand.dist ->
+            Map.add cand.coord (Tie cand.dist) grid
         | _ -> grid
       | false ->
-        Map.add c.coord (Closest (c.dist, coordId)) grid)
+        Map.add cand.coord (Closest { Dist = cand.dist; Id = coordId } ) grid)
     map
     coordGrid
 
 
-let caps = ['A' .. 'F']
-let lowers = ['a' .. 'f']
-//fold over the list of coords using ints as ids
-testCoords
+
+let lines = System.IO.File.ReadLines(@"C:\Users\bil91\code\Aoc2018\Day6\input.txt")
+let coords =
+  lines
+  |> Seq.map (fun x ->
+    printfn "%A" x
+    let components = x.Split([|", "|], StringSplitOptions.None)
+    (components.[0] |> int, components.[1] |> int))
+  |> List.ofSeq
+
+let extents = findExtents coords
+// let testCoords = [ (1, 1); (1, 6); (8, 3); (3, 4); (5, 5); (8, 9) ]
+// let extents = findExtents testCoords
+
+coords
 |> List.mapi (fun i c -> (i, (createCoordGrid extents c)))
 |> List.fold
   (fun map (i, cGrid) -> overlayCoordGrid map cGrid i)
   Map.empty
 |> Map.toList
-|> List.sortBy (fun ((x,y), _) -> (y, x))
-|> List.map (fun item ->
-  let (c, tv) = item
-  match tv with
-  | Closest (0, id) ->
-    printf "%s" (caps.[id].ToString())
-  | Closest (d, id) ->
-    printf "%s" (lowers.[id].ToString())
-  | Tie _ ->
-    printf "."
-  match c with
-  | (8, _) -> printfn ""
-  | _ -> printf ""
-  item)
 |> List.map snd
-|> List.filter (fun x ->
+|> List.choose (fun x ->
   match x with
-  | Closest _ -> true
-  | _ -> false)
-|> List.groupBy (fun (Closest (_, id)) -> id)
-|> List.map (fun (g, closests) -> (g, List.length closests))
+  | Closest cc -> Some cc
+  | _ -> None)
+|> List.groupBy (fun cc -> cc.Id)
+|> List.map (fun (id, closests) -> (id, List.length closests))
 |> List.map (fun x ->
   printfn "%A" x
   x)
-|> List.maxBy (fun (g, closestTilesCount) -> closestTilesCount)
+|> List.maxBy (fun (_, closestTilesCount) -> closestTilesCount)
 
 
+//to visualize
+// let testCoords = [ (1, 1); (1, 6); (8, 3); (3, 4); (5, 5); (8, 9) ]
+// let extents = findExtents testCoords
+// let caps = ['A' .. 'F']
+// let lowers = ['a' .. 'f']
+
+// testCoords
+// |> List.mapi (fun i c -> (i, (createCoordGrid extents c)))
+// |> List.fold
+//   (fun map (i, cGrid) -> overlayCoordGrid map cGrid i)
+//   Map.empty
+// |> Map.toList
+// |> List.sortBy (fun ((x,y), _) -> (y, x))
+// |> List.map (fun item ->
+//   let (c, tv) = item
+//   match tv with
+//   | Closest cc  when cc.Dist = 0 ->
+//     printf "%s" (caps.[cc.Id].ToString())
+//   | Closest cc ->
+//     printf "%s" (lowers.[cc.Id].ToString())
+//   | Tie _ ->
+//     printf "."
+//   match c with
+//   | (8, _) -> printfn ""
+//   | _ -> printf ""
+//   item)
+// |> List.map snd
+// |> List.choose (fun x ->
+//   match x with
+//   | Closest cc -> Some cc
+//   | _ -> None)
+// |> List.groupBy (fun cc -> cc.Id)
+// |> List.map (fun (g, closests) -> (g, List.length closests))
+// |> List.map (fun x ->
+//   printfn "%A" x
+//   x)
+// |> List.maxBy (fun (_, closestTilesCount) -> closestTilesCount)
 
 
-//create an empty grid to hold the results
-
-
-// let resultGrid = overlay
-//overlay the sequences on each other keeping only the shortest manhattan
-//distance for each square or ignoring  ties (by using None). Remember later coordinates
-//could possibly break ties
-// grid
-// |> Seq.iter (fun x -> printfn "%A" x)
