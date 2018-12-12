ba4e2babc2502a5f69a10dba08ebfefdfddabfc6
John Galambos <johnny.galambos@gmail.com>
2018-12-11 23:19:52 -0800

    Day 12 wip

--- Day12/Day12.fsx
+++ Day12/Day12.fsx
@@ -1,3 +1,14 @@
+open System
+
+type PotState = Empty | Full
+
+type Pot =
+  { Id: int
+    State: PotState }
+
+type PotList =
+  { Posts: pot list }
+
 let sampleState = "#..#.#..##......###...###"
 let sampleRules =
   [ "...## => #"
@@ -18,7 +29,7 @@ let sampleRules =
 
 let initialState = "#......##...#.#.###.#.##..##.#.....##....#.#.##.##.#..#.##........####.###.###.##..#....#...###.##"
 
-let rules = [
+let inputRules = [
   ".#.## => ."
   ".#### => ."
   "#..#. => ."
@@ -52,9 +63,10 @@ let rules = [
   "####. => #"
   "###.. => #"
 ]
-//we need to ensure that every rule only happens on the
 
-let getFives (input:string) =
+
+
+let getPots (input:string) =
   input
   |> Seq.mapi (fun i c ->
     match i with
@@ -64,7 +76,57 @@ let getFives (input:string) =
     | i when i = (input.Length - 2) -> input.[input.Length - 4 ..] + "." //second last element
     | _ -> input.[i-2..i+2 ])
 
-let pots = getFives sampleState
+let formatRules (input: string list) =
+  input
+  |> List.map (fun s -> s.Split([|" => "|], StringSplitOptions.None))
+  |> List.map (fun a -> (a.[0], a.[1]))
 
-pots
-|> Seq.iter (fun c -> printfn "%A" c)
+let applyRules rules pots =
+  pots
+  |> Seq.mapi (fun i pot ->
+    let result =
+      rules
+      |> Seq.tryFind (fun (pattern, grows) -> pattern = pot)
+    match result with
+    | Some (pattern, grows) ->
+      // printfn "matched %A with pot %A returning %A" pattern i grows
+      grows
+    | None ->
+      // printfn "no match for pot %A with pattern %A" i pot
+      ".") //assume nothing happens?
+  |> String.concat ""
+
+
+let rules = formatRules sampleRules
+let pots = getPots sampleState
+
+let rec appendEmptyPots (str:string) =
+  if str.[str.Length - 2..] <> ".." then
+    appendEmptyPots (str + ".")
+  else
+    str
+
+let rec prependEmptyPots (str:string) =
+  if str.[..1] <> ".." then
+    prependEmptyPots ("." + str)
+  else
+    str
+
+let appendPots str =
+  let str = appendEmptyPots str
+  prependEmptyPots str
+
+
+let finalResult =
+  { 1..20 }
+  |> Seq.fold (fun s i ->
+    let result = applyRules rules s
+    //append empty pots to string if necessary
+    let result = appendPots result
+    printfn "%A | gen %A" result i
+    let newPots = getPots result
+    newPots) pots
+
+
+
+
