cc762409ee9d353f3bb77c1c038e4510cd608811
John Galambos <johnny.galambos@gmail.com>
2018-12-15 10:50:59 -0800

    Day 15 wip. Dist grid does not consider unit locations

--- Day15/Day15.fsx
+++ Day15/Day15.fsx
@@ -1,3 +1,5 @@
+open System
+
 type UnitType = | Elf | Goblin
 
 type Unit =
@@ -12,13 +14,34 @@ type TileType =  | Wall | Open
 type GameState =
   { Map: TileType[,] }
 
+let isUnoccupied loc  units =
+  not (Seq.exists (fun u -> u.Location = loc) units)
+
+let inRangeTiles u gridDist units =
+  let enemyLocations =
+    units
+    |> Seq.filter (fun u' -> u'.Type <> u.Type)
+    |> Seq.map (fun u -> u.Location)
+
+  let unOccupiedAdjLoc =
+    seq { for el in enemyLocations do
+            let adjLoc =
+              Map.find el gridDist
+              |> Map.filter (fun l d -> d = 1 && isUnoccupied l units)
+              |> Map.toList
+              |> List.map (fun (l,_) -> l)
+            yield! adjLoc }
+  unOccupiedAdjLoc
+
+
 // let tickUnit u =
   ////find the nearest opponent
   ////try to move towards it
   ////attack if possible
 //let tick grid locations
 
-let genMap (input: string list) =
+
+let genMap input: string list) =
   let height = Seq.length input
   let width = Seq.length (input.[0])
   Array2D.init height width (fun i j ->
@@ -140,10 +163,17 @@ let getInterTileDist t1 t2 distGrid =
   |> Map.find  bottomRight
 
 let printDistGrid (map:TileType[,]) distGrid (i',j') =
+  let greekLowers = ['α'; 'β'; 'γ'; 'δ'; 'ε'; 'ζ'; 'η'; 'θ'; 'ι'; 'κ'; 'λ'; 'μ'; 'ν'; 'ξ'; 'ο'; 'π'; 'ρ'; 'σ'; 'τ'; 'υ'; 'φ'; 'χ'; 'ψ'; 'ω']
+  let lowers = ['a' .. 'z'] @ greekLowers
   for i in 0..Array2D.length1 map - 1 do
     for j in 0..Array2D.length2 map - 1 do
       if isOpenTile map (i,j) then
-        printf "%i" (getInterTileDist (i,j) (i',j') distGrid)
+        let dist = getInterTileDist (i,j) (i',j') distGrid
+        if dist < 10 then
+          printf "%i" dist
+        else
+          printf "%c" lowers.[dist]
+
       else
         printf "#"
     printfn ""
@@ -152,12 +182,19 @@ let printUnits units =
   units
   |> Seq.iter (fun u -> printfn "%A" u)
 
-let units = genUnits sample1
-// printUnits units
+// let map = genMap sample1
 
-let map = genMap sample1
+let lines =
+  System.IO.File.ReadLines("C:\Users\john\code\Aoc2018\Day15\input.txt")
+  |> List.ofSeq
+
+let units = genUnits lines
+printUnits units
+let map = genMap lines
+
 let distGrid = genDistGrid map
-printDistGrid map distGrid (3, 5)
+printDistGrid map distGrid (30, 17)
 
-
+let firstUnit = units |> Seq.head
+let irt = inRangeTiles firstUnit distGrid units
 
