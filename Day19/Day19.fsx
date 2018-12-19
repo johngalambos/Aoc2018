d9dbc7fa071f2a353853941232888eb94b962146
John Galambos <johnny.galambos@gmail.com>
2018-12-19 13:01:45 -0800

    Day 19 done

--- Day19/Day19.fsx
+++ Day19/Day19.fsx
@@ -2,91 +2,91 @@ open System.Text.RegularExpressions
 
 type Op =
   { Alias: string
-    Fn: int64 list -> int64 list -> int64 list }
+    Fn: int list -> int list -> int list }
 
 type Instruction =
   { Name: string
-    Fn: int64 list -> int64 list -> int64 list;
-    Input: int64 list }
+    Fn: int list -> int list -> int list
+    Input: int list }
 
-let addr reg ([op; i1; i2; out]: int64 list) =
+let addr reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] + reg.[i2 |> int]
   Array.toList regArr
 
-let addi reg ([op; i1; i2; out]: int64 list) =
+let addi reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] + i2
   Array.toList regArr
 
-let mulr reg ([op; i1; i2; out] : int64 list)=
+let mulr reg ([op; i1; i2; out] : int list)=
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] * reg.[i2 |> int]
   Array.toList regArr
 
-let muli reg ([op; i1; i2; out]: int64 list) =
+let muli reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] * i2
   Array.toList regArr
 
-let banr reg ([op; i1; i2; out]: int64 list) =
+let banr reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] &&& reg.[i2 |> int]
   Array.toList regArr
 
-let bani reg ([op; i1; i2; out]: int64 list) =
+let bani reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] &&& i2
   Array.toList regArr
 
-let borr reg ([op; i1; i2; out]: int64 list) =
+let borr reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] ||| reg.[i2 |> int]
   Array.toList regArr
 
-let bori reg ([op; i1; i2; out]: int64 list) =
+let bori reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int] ||| i2
   Array.toList regArr
 
-let setr reg ([op; i1; i2; out]: int64 list) =
+let setr reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- reg.[i1 |> int]
   Array.toList regArr
 
-let seti reg ([op; i1; i2; out]: int64 list) =
+let seti reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
   regArr.[out |> int] <- i1
   Array.toList regArr
 
-let gtir reg ([op; i1; i2; out]: int64 list) =
+let gtir reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
-  regArr.[out |> int] <- if i1 > reg.[i2 |> int] then 1L else 0L
+  regArr.[out |> int] <- if i1 > reg.[i2 |> int] then 1 else 0
   Array.toList regArr
 
-let gtri reg ([op; i1; i2; out]: int64 list) =
+let gtri reg ([op; i1; i2; out]: int list) =
   let regArr = List.toArray reg
-  regArr.[out |> int] <- if reg.[i1 |> int] > i2 then 1L else 0L
+  regArr.[out |> int] <- if reg.[i1 |> int] > i2 then 1 else 0
   Array.toList regArr
 
-let gtrr reg ([op; i1; i2; out] : int64 list)=
+let gtrr reg ([op; i1; i2; out] : int list)=
   let regArr = List.toArray reg
-  regArr.[out |> int] <- if reg.[i1 |> int] > reg.[i2 |> int] then 1L else 0L
+  regArr.[out |> int] <- if reg.[i1 |> int] > reg.[i2 |> int] then 1 else 0
   Array.toList regArr
 
-let eqir reg ([op; i1; i2; out] : int64 list)=
+let eqir reg ([op; i1; i2; out] : int list)=
   let regArr = List.toArray reg
-  regArr.[out |> int] <- if i1 = reg.[i2 |> int] then 1L else 0L
+  regArr.[out |> int] <- if i1 = reg.[i2 |> int] then 1 else 0
   Array.toList regArr
 
-let eqri reg ([op; i1; i2; out] : int64 list)=
+let eqri reg ([op; i1; i2; out] : int list)=
   let regArr = List.toArray reg
-  regArr.[out |> int] <- if reg.[i1 |> int] = i2 then 1L else 0L
+  regArr.[out |> int] <- if reg.[i1 |> int] = i2 then 1 else 0
   Array.toList regArr
 
-let eqrr reg ([op; i1; i2; out] : int64 list)=
+let eqrr reg ([op; i1; i2; out] : int list)=
   let regArr = List.toArray reg
-  regArr.[out |> int] <- if reg.[i1 |> int] = reg.[i2 |> int] then 1L else 0L
+  regArr.[out |> int] <- if reg.[i1 |> int] = reg.[i2 |> int] then 1 else 0
   Array.toList regArr
 
 let ops = [
@@ -118,43 +118,71 @@ let sampleProgram = [
   "seti 8 0 4"
   "seti 9 0 5"]
 
+(* the loop as imperative
+A <- 0
+B <- (10551430 essentially 1)
+C <- 10551430
+D <- 1
+E <- 1
+
+while true
+  B <- E * D (this is the sum of the factors of C)
+  if B = C then
+    B <- 1
+    A <- E + A
+  else
+    B <- 0
+  D <- D + 1 //increment D on every loop
+  if D > C then
+    B <- 1
+    E <- E + 1 //incremnt E only when D > C
+    if E > C then
+      B <- 1
+      failwith "we're done"
+    else
+      B <- 0
+      D <- 1 //reset D to one
+  else
+    B <- 0
+
+*)
 let actualProgram = [
-  "addi 5 16 5"
-  "seti 1 8 4"
-  "seti 1 5 3"
-  "mulr 4 3 1"
-  "eqrr 1 2 1"
-  "addr 1 5 5"
-  "addi 5 1 5"
-  "addr 4 0 0"
-  "addi 3 1 3"
-  "gtrr 3 2 1"
-  "addr 5 1 5"
-  "seti 2 5 5"
-  "addi 4 1 4"
-  "gtrr 4 2 1"
-  "addr 1 5 5"
-  "seti 1 2 5"
-  "mulr 5 5 5"
-  "addi 2 2 2"
-  "mulr 2 2 2"
-  "mulr 5 2 2"
-  "muli 2 11 2"
-  "addi 1 8 1"
-  "mulr 1 5 1"
-  "addi 1 18 1"
-  "addr 2 1 2"
-  "addr 5 0 5"
-  "seti 0 7 5"
-  "setr 5 0 1"
-  "mulr 1 5 1"
-  "addr 5 1 1"
-  "mulr 5 1 1"
-  "muli 1 14 1"
-  "mulr 1 5 1"
-  "addr 2 1 2"
-  "seti 0 0 0"
-  "seti 0 9 5" ]
+  "addi 5 16 5"   //0   Go to instruction 17
+  "seti 1 8 4"    //1   E <- 1 //set E to 1
+  "seti 1 5 3"    //2   D <- 1 //set D to 1
+  "mulr 4 3 1"    //3   B <- E * D //Start of Part 2 loops
+  "eqrr 1 2 1"    //4   if B = C then B <- 1 else B <- 0
+  "addr 1 5 5"    //5   F <- B + F
+  "addi 5 1 5"    //6   F <- F + 1
+  "addr 4 0 0"    //7   A <- E + A //for terimation E here will be 10551430 while A will be ? .. we only e
+  "addi 3 1 3"    //8   D <- D + 1 //for termination D here will be 10551430
+  "gtrr 3 2 1"    //9   if D > C then B <- 1 else B <- 0 for termination D here will be 10551431
+  "addr 5 1 5"    //10  F <- F + B
+  "seti 2 5 5"    //11  F <- 2
+  "addi 4 1 4"    //12  E <- E + 1 //for termination E here will be 10551430
+  "gtrr 4 2 1"    //13  if E > C then B <- 1 else B <- 0 //to trigger > than E  must be 10551431
+  "addr 1 5 5"    //14  F <- B + F (14,15,16) once E is greater than C go to 16
+  "seti 1 2 5"    //15  F <- 1
+  "mulr 5 5 5"    //16  F <- F * F (will have value 16 * 16 and the program will quit. What is the value of A? when this happens?)
+  "addi 2 2 2"    //17  Start of first program -> sets the value of C for part 2
+  "mulr 2 2 2"    //18
+  "mulr 5 2 2"    //19
+  "muli 2 11 2"   //20
+  "addi 1 8 1"    //21
+  "mulr 1 5 1"    //22
+  "addi 1 18 1"   //23
+  "addr 2 1 2"    //24
+  "addr 5 0 5"    //25
+  "seti 0 7 5"    //26
+  "setr 5 0 1"    //27
+  "mulr 1 5 1"    //28
+  "addr 5 1 1"    //29
+  "mulr 5 1 1"    //30
+  "muli 1 14 1"   //31
+  "mulr 1 5 1"    //32
+  "addr 2 1 2"    //33
+  "seti 0 0 0"    //34
+  "seti 0 9 5"]   //35
 
 
 
@@ -163,7 +191,7 @@ let instRegex = new Regex("^([a-z]+) (\d+) (\d+) (\d+)$")
 let parseInput input =
   let rmatch = instRegex.Match(input)
   let instr = rmatch.Groups.[1].Value
-  let values = [99L; (rmatch.Groups.[2].Value |> int64); (rmatch.Groups.[3].Value |> int64); (rmatch.Groups.[4].Value |> int64) ]
+  let values = [99; (rmatch.Groups.[2].Value |> int); (rmatch.Groups.[3].Value |> int); (rmatch.Groups.[4].Value |> int) ]
   { Name = instr
     Fn = (List.find (fun op -> op.Alias = instr) ops).Fn
     Input = values }
@@ -174,27 +202,45 @@ let instructionsArray =
   |> Array.ofSeq
 
 
-let rec run (input:Instruction []) ip (regs: int64 list) iteration  =
+let rec run (input:Instruction []) ip (regs: int list) iteration  =
   let nextInputPtr = regs.[ip]
-  let instruction = input.[nextInputPtr |> int]
-  printfn  "instruction at %i" nextInputPtr
+  let instruction = input.[nextInputPtr]
   let output = instruction.Fn regs instruction.Input
-  printfn "output %A" output
-  if output.[ip] > ((input.Length - 1) |> int64) || iteration > 100 then
+  // printfn "output %A" output
+  if output.[ip] > ((input.Length - 1)) || iteration > 100 then
     printfn "bailing out early because tried pointing to instruction %A" output.[ip]
     output
   else
-    let incrementedPointer = output |> Array.ofList |> (fun arr -> (arr.[ip] <- arr.[ip] + 1L); arr) |> Array.toList
-    if incrementedPointer.[ip] > (input.Length - 1 |> int64) then
+    let incrementedPointer = output |> Array.ofList |> (fun arr -> (arr.[ip] <- arr.[ip] + 1); arr) |> Array.toList
+    if incrementedPointer.[ip] > (input.Length - 1) then
       printfn "bailing out early because tried pointing to instruction %A" incrementedPointer.[ip]
       incrementedPointer
     else
       if iteration % 10000000 = 0 then
         printfn "iteration %i reg0: %i" iteration regs.[0]
       else ()
+      printfn "(%i, %s) in:%A out:%A" nextInputPtr instruction.Name regs incrementedPointer
       run input ip incrementedPointer (iteration + 1)
 
 
 
 let answer =
-  run instructionsArray 5 [1L; 0L; 0L; 0L; 0L; 0L] 0
+  run instructionsArray 5 [1; 0; 0; 0; 0; 0] 0
+  //the first teim we hit the constraint
+  // run instructionsArray 5 [0; 10551430; 10551430; 10551430; 1; 3] 0
+  //the second time we hit the constraint
+  // run instructionsArray 5 [1; 10551430; 10551430; 10551430; 2; 3] 0
+  // run instructionsArray 5 [0; 10551430; 10551430; 10551430; 1; 3] 0
+
+let n = 10551430
+
+let factors =
+  [ 1..(10551430)]
+  |> Seq.filter (fun i -> 10551430 % i = 0)
+// I guessed 18992592 which was rong
+let answer =
+  factors
+  |> Seq.sum
+// factors
+// |> Seq.sum
+// 18992592 is the sum of the factors
